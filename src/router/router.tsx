11adfc7ac04849782e4e3e63e333771cf5f0b1fc
GruescuDanziel <gruescu.ipatie@gmail.com>
2023-04-13 14:50:11 +0300

    Added the routing feature

---
+++ src/router/router.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import TabletApp from "../components/TabletApp/TabletApp";
+import Admin from "../components/Admin/Admin";
+
+const MainRouter = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="admin" element={<Admin />} />
+                <Route path="/" element={<TabletApp />} />
+            </Routes>
+        </BrowserRouter>
+    );
+};
+
+export default MainRouter;
