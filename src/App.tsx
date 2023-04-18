d1bfbc2bcc5346b2ce455565b85dee2dfdd8420a
GruescuDanziel <gruescu.ipatie@gmail.com>
2023-04-18 14:35:59 +0300

    The Toaster has been completed and integrated with the latest version of main

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,18 @@ import "./App.css";
 import GlobalTheme from "./context/GlobalThemes";
 import { ThemeProvider } from "@mui/material/styles";
 import MainRouter from "./router/router";
+import { SnackbarProvider } from "notistack";
 
 function App() {
     return (
         <ThemeProvider theme={GlobalTheme}>
-          <MainRouter/>
+            <SnackbarProvider
+                anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+                style={{display:"flex", alignItems:"start" }}
+                autoHideDuration={100000}
+            >
+                <MainRouter />
+            </SnackbarProvider>
         </ThemeProvider>
     );
 }
--- src/components/TabletApp/TabletApp.tsx
+++ src/components/TabletApp/TabletApp.tsx
@@ -1,15 +1,22 @@
 import { Box,Container} from "@mui/system";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import COLORS from "../../constants/CustomColors";
 import LeftSide from "./LeftSide/LeftSide";
 import RightSide from "./RightSide/RightSide";
 import Grid from "@mui/material/Grid";
+import {spawnToast} from "../../utils/Toast";
+import {Typography} from "@mui/material";
 const TabletApp = () => {
     const colorStates = [COLORS.GREEN, COLORS.YELLOW, COLORS.RED];
 
     const [availability, setAvailability] = useState(1);
     const [roomName, setRoomName] = useState("Focus Room");
-    
+
+  useEffect(() => {
+    spawnToast("You have succeded","Your booking was made",true)
+    spawnToast("Something went wrong","Your booking has not been made",false)
+    }, [])
+   
     return (
         <Grid sx={{backgroundColor:colorStates[availability]}} flexDirection="row"  container>
             <Grid  item xs={5}>
