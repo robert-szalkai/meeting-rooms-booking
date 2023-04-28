dcd34566296249ed7b0aa445893af7c1ffbab249
thedeath154 <53562885+thedeath154@users.noreply.github.com>
2023-04-28 04:02:21 +0300

    version 1.0.2

--- src/components/Charts/Chart.tsx
+++ src/components/Charts/Chart.tsx
@@ -1,14 +1,10 @@
-import React, { FunctionComponent, useState } from "react";
-import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid } from "recharts";
+import React, { useState, useEffect } from "react";
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid } from "recharts";
 import IconButton from "@mui/material/IconButton";
 import Box from "@mui/material/Box";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import { Link } from "react-router-dom";
-import data from "../../Charts.json";
 import { getMeetingsData } from "../../api/getRequests";
-interface iChart {
-    data: { name: string; uv: number }[];
-}
 
 const Chart = ({ data }: any) => {
     console.log(data);
@@ -21,8 +17,8 @@ const Chart = ({ data }: any) => {
             </Box>
             <Box sx={{ flexGrow: 1 }}>
                 <BarChart
-                    width={1450}
-                    height={770}
+                     width={690}
+                     height={270}
                     data={data}
                     margin={{
                         top: 20,
@@ -75,25 +71,46 @@ const AllCharts = () => {
             setMeetingsRoom(meetings);
         });
     };
-
     if(meetings.length == 0)b();
 
-    return (
-        <Box sx={{ display: "flex", flexDirection: "column" }}>
-            <Box sx={{ display: "flex", alignItems: "center", mt: 1, ml: 1 }}>
-                <IconButton component={Link} to="http://localhost:3000/hub">
-                    <ArrowBackIcon />
-                </IconButton>
-            </Box>
-            <div>
-                <h1>All Charts</h1>
-                <div>
-                    <h2>Rooms</h2>
-                    <Chart data={meetingsRoom} />
-                </div>
-            </div>
-        </Box>
-    );
-};
+    return (    
+         <Box sx={{ display: "flex", flexDirection: "column" }}>
+         <Box sx={{ display: "flex", alignItems: "center", mt: 1, ml: 1 }}>
+             <IconButton component={Link} to="http://localhost:3000/hub">
+                 <ArrowBackIcon />
+             </IconButton>
+         </Box>
+           <div style={{ display: "grid", gridTemplateColumns: "1fr 1fr", gridGap: "20px", gridAutoRows: "50%" }}>
+             <div>
+               <h2>Rooms</h2>
+               <Chart data={meetingsRoom} />
+             </div>
+           {/*  <div>
+            <h2>Weeks</h2>
+               <Chart data={weekData} />
+             </div>
+         <div style={{ gridColumn: "1 / 3" }}>
+               <h2>Months</h2>
+               <BarChart
+                 width={1430}
+                 height={300}
+                 data={monthData}
+                 margin={{
+               top: 20,
+                   right: 30,
+                   left: 20,
+                   bottom: 5,
+                 }}
+           >
+                 <CartesianGrid strokeDasharray="3 3" />
+                 <XAxis dataKey="name" />
+                 <YAxis />
+                 <Bar dataKey="value" fill="#8884d8" label={{ position: "top" }} />
+               </BarChart>
+             </div> */}
+           </div>
+           </Box>
+         );
+       };
 
 export default AllCharts;
